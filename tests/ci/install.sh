400e0efd3a69cc80186a7d99c34bc7bbc38b5135
Serghei Iakovlev <sadhooklay@gmail.com>
2015-01-25 00:43:11 +0200

    Added CI output

--- tests/ci/install.sh
+++ tests/ci/install.sh
@@ -2,26 +2,36 @@
 
 INSTALL_DIR=/usr/local/src/zephir
 
-echo -e "\n\e[0;32mBuild and install Zephir from https://github.com/phalcon/zephir\e[0m [\e[1;33m${ZEPHIR_VER}\e[0m]"
+echo -e "\n\e[0;32mCreate install dir:\e[0m [\e[1;33m${INSTALL_DIR}\e[0m]"
 echo -e ""
 
-sudo -s
+sudo mkdir ${INSTALL_DIR}
 
-mkdir ${INSTALL_DIR}
-chown ${USER} ${INSTALL_DIR}
+echo -e "\n\e[0;32mChange permission to:\e[0m [\e[1;33m${USER}\e[0m]"
+echo -e ""
+
+sudo chown ${USER} ${INSTALL_DIR}
+
+echo -e "\n\e[0;32mClone Zephir from https://github.com/phalcon/zephir\e[0m [\e[1;33m${ZEPHIR_VER}\e[0m]"
+echo -e ""
+
 git clone https://github.com/phalcon/zephir.git ${INSTALL_DIR}
-
 cd ${INSTALL_DIR}
 git fetch --all
 git checkout -b ${ZEPHIR_VER} ${ZEPHIR_VER}
 git submodule update --init
 
+echo -e "\n\e[0;32mBuild and install JSON-C...\e[0m"
+echo -e ""
+
 cd json-c
 sh autogen.sh
 ./configure
-make && make install
+make && sudo make install
+
+
+echo -e "\n\e[0;32mBuild and install Zephir...\e[0m"
+echo -e ""
 
 cd ..
 ./install -c
-
-exit
