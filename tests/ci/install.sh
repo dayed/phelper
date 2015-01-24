4c46aa4b3215d8289eafa17c5f5cadcfdf7b6342
Serghei Iakovlev <sadhooklay@gmail.com>
2015-01-25 00:28:08 +0200

    Improved before_script CI

--- tests/ci/install.sh
+++ tests/ci/install.sh
@@ -5,18 +5,23 @@ INSTALL_DIR=/usr/local/src/zephir
 echo -e "\n\e[0;32mBuild and install Zephir from https://github.com/phalcon/zephir\e[0m [\e[1;33m${ZEPHIR_VER}\e[0m]"
 echo -e ""
 
-sudo mkdir $INSTALL_DIR
-sudo chown $USER $INSTALL_DIR
-git clone https://github.com/phalcon/zephir.git $INSTALL_DIR
+sudo -s
 
-cd $INSTALL_DIR
-git checkout ${ZEPHIR_VER}
+mkdir ${INSTALL_DIR}
+chown ${USER} ${INSTALL_DIR}
+git clone https://github.com/phalcon/zephir.git ${INSTALL_DIR}
+
+cd ${INSTALL_DIR}
+git fetch --all
+git checkout -b ${ZEPHIR_VER} ${ZEPHIR_VER}
 git submodule update --init
 
 cd json-c
 sh autogen.sh
 ./configure
-make && sudo make install
+make && make install
 
 cd ..
-sudo ./install -c
+./install -c
+
+exit
