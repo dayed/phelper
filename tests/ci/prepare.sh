8564f8140c5c9ea53f892c99313f51bef467a2fd
Serghei Iakovlev <sadhooklay@gmail.com>
2015-01-25 01:39:22 +0200

    Debug Travis CI part 4

--- tests/ci/prepare.sh
+++ tests/ci/prepare.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-declare -r CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")"../../ && pwd)"
+declare -r CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")"/../../ && pwd)"
 cd "${CURRENT_DIR}"
 echo -e "\e[0;32mCurrent dir:\e[0m [\e[1;33m${CURRENT_DIR}\e[0m]\n"
 
