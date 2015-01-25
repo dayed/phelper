1aa61eb474513f3d9baa125e24e5947f374d40f4
Serghei Iakovlev <sadhooklay@gmail.com>
2015-01-25 02:10:10 +0200

    Debug Travis CI part 10

--- tests/ci/prepare.sh
+++ tests/ci/prepare.sh
@@ -4,12 +4,10 @@ declare -r CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")"/../../ && pwd)"
 cd "${CURRENT_DIR}"
 echo -e "\e[0;32mCurrent dir:\e[0m [\e[1;33m${CURRENT_DIR}\e[0m]\n"
 
-echo -e "\e[0;32mGenerate C code from the Zephir code\e[0m\n"
-sudo zephir build
+echo -e "\e[0;32mBuild Phelper extension: \e[0m\n"
+sudo zephir build && echo -e '[\033[0;32mBuild Done!\033[0m]\n' || echo -e '[\033[0;31mBuild Fail!\033[0m]'; exit 1
 
-echo -e "\e[0;32mBuild Done!\e[0m\n"
-
-# phpenv config-add ./tests/ci/phelper.ini
-# phpenv rehash
+phpenv config-add ./tests/ci/phelper.ini
+phpenv rehash
 
 cd "${CURRENT_DIR}"
