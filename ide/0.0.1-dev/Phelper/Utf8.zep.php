90baded7ee737fd0289c5cf9ec174659c9eeffff
Serghei Iakovlev <sadhooklay@gmail.com>
2015-01-25 02:36:18 +0200

    Added IDE stubs

---
+++ ide/0.0.1-dev/Phelper/Utf8.zep.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Phelper;
+
+class Utf8
+{
+    /**
+     * Does the server support UTF-8 natively?
+     *
+     * @var boolean
+     */
+    protected $_mbSupport = false;
+    /**
+     * Encoding using in mb_* functions
+     *
+     * @var string
+     */
+    protected $_encoding = "utf-8";
+
+    /**
+     * Class constructor
+     *
+     * @param string $encoding Encoding using in mb_* functions
+     */
+	public function __construct($encoding = "utf-8") {}
+
+    /**
+     * Tests whether a string contains only 7-bit ASCII bytes.
+     * This is used to determine when to use native functions or UTF-8 functions.
+     * <code>
+     * $ascii = $utf->isAscii($str);
+     * </code>
+     *
+     * @param mixed $text String or array of strings to check
+     * @return bool 
+     */
+	public function isAscii($text) {}
+
+    /**
+     * Get string length.
+     * This is a UTF8-aware version of [strlen](http://php.net/strlen).
+     * <code>
+     * $length = $utf->strlen($str);
+     * </code>
+     *
+     * @param string $text String being measured for length
+     * @return int 
+     */
+	public function strlen($text) {}
+
+    /**
+     * Get part of a UTF-8 string.
+     * This is a UTF8-aware version of [substr](http://php.net/substr).
+     * <code>
+     * $sub = $utf->substr($string, $offset);
+     * </code>
+     *
+     * @param string $text 
+     * @param int $offset 
+     * @param mixed $length 
+     * @return string 
+     */
+	public function substr($text, $offset, $length = null) {}
+
+}
